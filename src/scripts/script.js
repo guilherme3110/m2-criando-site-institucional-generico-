e7c45c9f50b96bf0e48802c2a416f94c36e65a4a
Guilherme Scaramuzzi <guilherme311094@hotmail.com>
2023-01-31 20:19:08 -0300

    projeto finalizado

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,1 +1,24 @@
-/* ------------------------------- Coloque sua lógica aqui ------------------------------- */
+function handleModal(){
+    const button = document.querySelector('#showMessage')
+    const modalContainer = document.querySelector('#modalContainer')
+
+    button.addEventListener('click', () => {
+        modalContainer.showModal()
+
+        closeModal()
+    })
+}
+
+
+
+
+function closeModal(){
+    const button = document.querySelector('#closeModal')
+    const modalContainer = document.querySelector('#modalContainer')
+
+    button.addEventListener('click', () => {
+        modalContainer.close()
+    })
+}
+
+handleModal()
